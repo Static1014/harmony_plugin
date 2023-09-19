1404fa598e36adc11a93d3d81fd011fd2825e804
Static1014 <420048248@qq.com>
2023-09-19 18:02:51 +0800

    1. 补充changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 ## 0.0.1
 
-* 
+1. Complete harmony utils method;
+2. Open source protocol supplement;
--- test/harmony_plugin_method_channel_test.dart
+++ test/harmony_plugin_method_channel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:harmony_plugin/harmony_plugin_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelHarmonyPlugin platform = MethodChannelHarmonyPlugin();
   const MethodChannel channel = MethodChannel('com.static4u.harmony_plugin');
 
   setUp(() {
